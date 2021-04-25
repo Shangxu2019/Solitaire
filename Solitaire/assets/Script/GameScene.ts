553b6f1db10f38a4ecfecde17b2714a102c94881
MrShang2021 <1728453756@qq.com>
2021-04-25 10:34:34 +0800

    确定发牌、手牌、玩牌区位置

--- Solitaire/assets/Script/GameScene.ts
+++ Solitaire/assets/Script/GameScene.ts
@@ -5,8 +5,8 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class GameScene extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+    // @property(cc.Label)
+    // label: cc.Label = null;
 
     private m_gameCtrl:GameCtrl = null;
 
@@ -20,7 +20,7 @@ export default class GameScene extends cc.Component {
     }
     start () {
         // init logic
-        this.label.string = '这里是游戏场景';
+        //this.label.string = '这里是游戏场景';
         console.log(">>> GameScene:start");
 
         this.m_gameCtrl = new GameCtrl();
